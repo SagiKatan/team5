0cf0428e08ff0a95c70496550cbd2dc989233f55
nismanbar <nismanbar@gmail.com>
2024-12-15 18:06:26 +0200

    test3

--- lesson_1.py
+++ lesson_1.py
@@ -1,6 +1,14 @@
+IsItWorking: bool = bool(input("is it?"))
+
+
 def test():
-    print("Its working")
-    print("WWWWW")
+
+    if IsItWorking:
+        print("Its working")
+        print("WWWWW")
+
+    if not IsItWorking:
+        print(":c")
 
 
 test()
